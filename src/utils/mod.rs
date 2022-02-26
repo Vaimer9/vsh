9e77f6f3d4bcac5adf8a380410304233d268a1ce
Vaimer9 <vaim3r9@gmail.com>
2022-02-26 18:30:42 +0530

    Changed file structure and added utils folder

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,17 +2,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+*/
 
-mod builtins;
-mod command;
-mod eval;
-mod prompt;
-mod repl;
+
+mod highlight;
 mod utils;
 
-use repl::Repl;
-
-fn main() {
-    Repl::new().start_shell().unwrap();
-}
+pub use {
+    highlight::*,
+    utils::*
+};
